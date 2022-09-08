ff579753a810690317173678b5a63e522eccaf07
Spencer Bernard <105369089+sjbern@users.noreply.github.com>
2022-09-07 19:55:16 -0600

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from users import views as user_views
 admin.site.site_url = 'http://localhost:8000/home/'  # changes the 'View Site' link to homepage, update once hosted
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #these links are for independet folders
@@ -18,3 +20,6 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
