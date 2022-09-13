5cb71152eab66f741c5a47ef36deece8a4353c1b
Spencer Bernard <105369089+sjbern@users.noreply.github.com>
2022-09-13 07:53:38 -0600

    Add files via upload

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +9,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta: #nested namespace to keep configurations in one place
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta: #allows user to update profile information
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm): #allows user to update image
+    class Meta:
+        model = Profile
+        fields = ['image']
