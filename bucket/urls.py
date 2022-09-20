33198c426d77ed9458cf75eaf67143e666258be0
Spencer Bernard <105369089+sjbern@users.noreply.github.com>
2022-09-20 08:21:45 -0600

    Update urls.py

--- bucket/urls.py
+++ bucket/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from .views import (
+                    ZoneCreateView,
+                   )
 
 
 urlpatterns = [
     path('', views.home, name='bucket-home'),
     path('bucketSet/', views.bucketSet, name='bucket-settings'),
+    path('bucket/new/', ZoneCreateView.as_view(), name='zone-create'),
 
 ]
