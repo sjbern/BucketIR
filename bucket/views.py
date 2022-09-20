9e3a4a685979097d33f20716829f742f2129bd5d
Spencer Bernard <105369089+sjbern@users.noreply.github.com>
2022-09-20 08:22:01 -0600

    Update views.py

--- bucket/views.py
+++ bucket/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Create
+
+from blog.models import Post
+from .models import Zone
+from django.views.generic import (ListView, 
+                                  DetailView, 
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def home(request):
     context = {
-        'Create':Create.objects.all()
+        'Zone':Zone.objects.all()
     }
     return render(request, 'bucket/bucket.html', context)
 
 def bucketSet(request):
     return render(request, 'bucket/bucket.html', {'title':'Bucket Settings'})
+
+class ZoneListView(ListView):
+    model = Post
+    template_name = 'blog/blog.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class ZoneCreateView(LoginRequiredMixin, CreateView):
+    model = Zone
+    fields = ['_ID', 'description', 'flow', 'area']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
